0eb8395209bec12dd1c6c9618badb7b7453190dc
h-atatsuki <sumiya.sub01@gmail.com>
2024-12-28 14:25:50 +0900

    fix

--- lib/server/update.dart
+++ lib/server/update.dart
@@ -8,7 +8,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'update.g.dart';
 
-const v = 'v0.2.4';
+const v = 'v0.2.5';
 
 @Riverpod(keepAlive: true)
 class LatestVersion extends _$LatestVersion {
